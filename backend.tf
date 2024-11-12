3e5f1a2131f3fdffd0d4870008023308c7b73a66
skie2 <38520950+skie2@users.noreply.github.com>
2024-11-12 16:12:20 +0000

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.1.0"
   backend "remote" {
     hostname = "app.terraform.io"
-    organization = "skx_group"
+    organization = "ace-iac-day-zer"
     workspaces {
       name = "ace-iac-day-zero"
     }
